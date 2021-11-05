7f2357d9f4d868574b5d2f76f39b4cf5d5fa7d1b
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-11-05 16:34:20 +0200

    added files and folders to my vue 3 project

--- src/components/TodoListApp.vue
+++ src/components/TodoListApp.vue
@@ -1,18 +1,128 @@
 <template>
-  <div>
-    Hey, DSV
+  <div class="container">
+    <h2 class="text-center mt-5">My Vue TodoListApp</h2>
+    <!-- Input -->
+    <div class="d-flex">
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Enter Task"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        SUBMIT
+      </button>
+    </div>
+    <!-- Task Table -->
+    <table
+      class="table table-success table-striped table-bordered border-dark mt-5"
+    >
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ finished: task.status === 'finished' }">{{
+              task.name
+            }}</span>
+          </td>
+          <td style="width: 120px">
+            <span
+              @click="changeStatus(index)"
+              class="pointer"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-warning': task.status === 'in-progress',
+                'text-success': task.status === 'finished',
+              }"
+            >
+              {{ firstCharUpper(task.status) }}
+            </span>
+          </td>
+          <td>
+            <div class="text-center" @click="editTask(index)">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      availableStatuses: ["to-do", "in-progress", "finished"],
+      tasks: [
+        {
+          name: "Steal Bananas From The Store",
+          status: "to-do",
+        },
+        {
+          name: "Eat 1kg chocolate in 1 hour.",
+          status: "in-progress",
+        },
+      ],
+    };
+  },
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+      if (this.editedTask == null) {
+        this.tasks.push({
+          name: this.task,
+          status: "to-do",
+        });
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
+      this.task = "";
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    changeStatus(index) {
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+    },
+    firstCharUpper(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pointer {
+  cursor: pointer;
+}
+.finished {
+  text-decoration: line-through;
+}
 </style>
